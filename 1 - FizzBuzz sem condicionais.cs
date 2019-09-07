94961cd1a64329c8f19877fadf12f2b359550a3e
Ruan Carlos CS <ruancarlos14@hotmail.com>
2019-09-07 14:27:26 -0500

    Update 1 - FizzBuzz sem condicionais.cs

--- 1 - FizzBuzz sem condicionais.cs
+++ 1 - FizzBuzz sem condicionais.cs
@@ -1,3 +1,13 @@
+/*
+ Fizzbuzz sem if/switch/ternário
+
+Fizzbuzz é uma função que recebe um número e retorna "fizz" se o número for divisível por 3, 
+"buzz"  se for divisível por 5 e "fizzbuzz" se for divisível por 3 e 5. Se o número não for divisível 
+nem por 3 e nem por 5, retorna o número.
+
+Faça a função fizzbuzz sem usar if, switch ou  ternário
+*/
+
 using System;
 
 namespace Logikoz.Desafios
