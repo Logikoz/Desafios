32eae940664f0aec965c1279093a256dd507ec64
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-06 18:43:23 -0500

    Update 4 - Debounce.cs

--- 4 - Debounce.cs
+++ 4 - Debounce.cs
@@ -18,25 +18,66 @@ fn() // "hello world"
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Logikoz.Desafios
 {
-    internal delegate void Call(string x, int y = 1000);
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Debounce.Send(new NewAction(() => Console.WriteLine("c# the best"), Guid.Parse("38C7D896-0C11-4D23-A257-A7DF749D2B13")), 3000);
+                Debounce.Send(new NewAction(() => Console.WriteLine("outra action"), Guid.Parse("195A1A0F-D61D-4A32-AF65-67E39158785E")), 500);
+                Debounce.Send(new NewAction(() => Console.WriteLine("iae"), Guid.Parse("1489D2D5-C88D-4801-B322-A3478D25F04E")), 1000);
+            }
+        }
+    }
 
     internal class Debounce
     {
-        static readonly DateTime[] times = new DateTime[2];
-        public static void Message1(string msg, int delay) => Send(msg, delay, 0);
-        public static void Message2(string msg, int delay) => Send(msg, delay, 1);
+        private static readonly List<DateTime> times = new List<DateTime>();
+        private static readonly List<NewAction> actions = new List<NewAction>();
 
-        private static void Send(string msg, int delay, byte i)
+        internal static void Send(NewAction action, int delay)
         {
             DateTime time = DateTime.Now;
-            if ((time.Ticks - times[i].Ticks) / TimeSpan.TicksPerMillisecond > delay)
+            if (!actions.Any(a => a.Id == action.Id))
             {
-                Console.WriteLine(msg);
-                times[i] = time;
+                NewAction(action, time);
+                return;
             }
+            if (actions.SingleOrDefault(a => a.Id == action.Id) is NewAction currentAction)
+            {
+                int index = actions.IndexOf(currentAction);
+                if ((time.Ticks - times[index].Ticks) / TimeSpan.TicksPerMillisecond > delay)
+                {
+                    times.RemoveAt(index);
+                    actions.RemoveAt(index);
+                    NewAction(action, time);
+                }
+            }
+
+            static void NewAction(NewAction action, DateTime time)
+            {
+                action.Action.Invoke();
+                times.Add(time);
+                actions.Add(action);
+            }
+        }
+    }
+
+    internal class NewAction
+    {
+        internal Guid Id { get; }
+        internal Action Action { get; }
+
+        public NewAction(Action action, Guid guid)
+        {
+            Id = guid;
+            Action = action;
         }
     }
 }
